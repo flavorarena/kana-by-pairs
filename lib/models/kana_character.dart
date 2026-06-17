bbddc57623796aac370f8e5a3ec9accf617fd82a
Kaileigh <flavorarena@gmail.com>
2026-06-16 21:02:06 -0500

    Initial commit

---
+++ lib/models/kana_character.dart
@@ -0,0 +1,15 @@
+class KanaCharacter {
+  final String hiragana;
+  final String katakana;
+  final String romaji;
+  final String hMnemonic;
+  final String kMnemonic;
+
+  KanaCharacter({
+    required this.hiragana,
+    required this.katakana,
+    required this.romaji,
+    required this.hMnemonic,
+    required this.kMnemonic,
+  });
+}
